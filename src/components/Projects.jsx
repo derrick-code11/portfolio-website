fa1a95c2f4294e7abb4b49e0b2ecf1ea3920a2e3
derrick-code11 <agyemangderrick777@gmail.com>
2024-06-20 13:33:46 -0400

    added swiper UI component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,64 @@
 import projectData from "../projects.json";
 import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 import Project from "./Project";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 import "../styles/Projects.css";
 
 const Projects = () => {
   return (
-    <>
+<>
       <section id="projects">
         <p className="section__text__p1">Browse My Recent</p>
         <h1 className="title">Projects</h1>
         <div className="skills-details-container">
-          <div class="about-containers">
-            {projectData.map((project)=>(
-                <Project
-                key={project.id}
-                media={project.media}
-                name={project.name}
-                glink={project["github-link"]}
-                dlink={project["demo-link"]}
-                ></Project>
-            ))}
+          <div className="about-containers">
+            <Swiper
+              effect={"coverflow"}
+              grabCursor={true}
+              centeredSlides={true}
+              loop={true}
+              slidesPerView={"auto"}
+              coverflowEffect={{
+                rotate: 0,
+                stretch: 0,
+                depth: 100,
+                modifier: 2.5,
+              }}
+              pagination={{ el: ".swiper-pagination", clickable: true }}
+              navigation={{
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+                clickable: true,
+              }}
+              modules={[EffectCoverflow, Pagination, Navigation]}
+              className="swiper_container"
+            >
+              {projectData.map((project) => (
+                <SwiperSlide key={project.id}>
+                  <Project
+                    media={project.media}
+                    name={project.name}
+                    glink={project["github-link"]}
+                    dlink={project["demo-link"]}
+                  ></Project>
+                </SwiperSlide>
+              ))}
+
+              <div className="slider-controler">
+                <div className="swiper-button-prev slider-arrow">
+                  <ion-icon name="arrow-back-outline"></ion-icon>
+                </div>
+                <div className="swiper-button-next slider-arrow">
+                  <ion-icon name="arrow-forward-outline"></ion-icon>
+                </div>
+                <div className="swiper-pagination"></div>
+              </div>
+            </Swiper>
           </div>
         </div>
         <a className="arrow" href="#contact">
