9d4680132c1ea0e00252375304c549c76c8f6df5
derrick-code11 <agyemangderrick777@gmail.com>
2024-06-19 21:02:54 -0400

    made changes to files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import "./App.css";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Hamburger from "./components/Hamburger";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Header></Header>
+      <Profile></Profile>
+      <About></About>
+      <Skills></Skills>
+      <Projects></Projects>
     </>
   );
 }
