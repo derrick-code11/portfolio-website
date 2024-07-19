3443abe69b1437cb6872fbaea97978c8349878fc
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-19 09:59:32 -0400

    changes for view on smaller screens

--- src/App.jsx
+++ src/App.jsx
@@ -10,44 +10,18 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
-  const [isScreenAllowed, setIsScreenAllowed] = useState(true);
-
-  useEffect(() => {
-    const checkScreenSize = () => {
-      if (window.innerWidth < 768 || window.innerHeight < 768){
-        setIsScreenAllowed(false);
-      } else {
-        setIsScreenAllowed(true);
-      }
-    };
-
-    checkScreenSize();
-
-    window.addEventListener("resize", checkScreenSize); 
-
-    return () => window.removeEventListener("resize", checkScreenSize); // Cleanup listener on unmount
-  }, []);
-
   return (
     <main id="app">
-      {isScreenAllowed ? (
-        <>
-          <Header />
-          <Hamburger />
-          <Profile />
-          <About />
-          <Skills />
-          <Projects />
-          <Contact />
-          <Footer />
-        </>
-      ) : (
-        <div className="restricted-message">
-          <p>
-            This application is not available on screens smaller than 768px.
-          </p>
-        </div>
-      )}
+      <>
+        <Header />
+        <Hamburger />
+        <Profile />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </>
     </main>
   );
 }
